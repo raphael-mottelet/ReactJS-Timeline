dda21f191ec07a1d3583c1eaed3be5887ffefb57
raphael-mottelet <72501205+raphael-mottelet@users.noreply.github.com>
2024-02-16 14:06:09 +0100

    fully functionnal basic timeline but non-responsive

--- basic-timeline/frontend/src/components/TimeLine/Timeline.jsx
+++ basic-timeline/frontend/src/components/TimeLine/Timeline.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import './timeline.css';
+import './testcss.css';
 
 const Timeline = ({ events }) => {
   return (
     <div className="timeline">
       <div className="timeline-line"></div>
+
       <div className="timeline-items">
         {events.map((event, index) => (
           <div key={index} className={`timeline-item ${index % 2 === 0 ? 'left' : 'right'}`}>
             <div className="timeline-item-content">
+              <div className="timeline-dot"></div>
               <span className="timeline-item-date">{event.date}</span>
               <h3 className="timeline-item-title">{event.title}</h3>
               <p className="timeline-item-description">{event.description}</p>
